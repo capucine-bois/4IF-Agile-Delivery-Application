950c372b3a64d0128991f23a1d20e8355907bc9d
abrissard <alexis.brissard@insa-lyon.fr>
2021-11-30 16:53:08 +0100

    :zap: TSP performance improvements

--- src/main/java/model/MinIter.java
+++ src/main/java/model/MinIter.java
@@ -25,37 +25,35 @@ public class MinIter implements Iterator<Integer> {
      * Vertices are traversed in the same order as in <code>unvisited</code>
      * @param unvisited the unvisited vertex
      * @param currentVertex the current vertex
-     * @param g
+     * @param g the graph containing all the costs
      */
     public MinIter(Collection<Integer> unvisited, int currentVertex, Graph g){
         this.candidates = new Integer[unvisited.size()];
         for (Integer s : unvisited){
             // if we found a delivery Address and the pick-up linked is in the unvisited nodes,
             // we don't add the delivery address to the candidates
-            if (g.isArc(currentVertex, s) && !(s%2==0 && unvisited.contains(s-1))) {
+            if (g.isArc(currentVertex, s) && !(s % 2 == 0 && unvisited.contains(s - 1))) {
                 candidates[nbCandidates++] = s;
             }
         }
 
-        System.out.println("Candidates unsorted : \n" + Arrays.toString(candidates));
+        //System.out.println("Candidates unsorted : \n" + Arrays.toString(candidates));
         //Arrays.sort(this.candidates, Comparator.comparingDouble(o -> -g.getCost(currentVertex, o)));
 
-        Arrays.sort(candidates, new Comparator<Integer>() {
-            public int compare(Integer o1, Integer o2) {
-                if (o1 == null && o2 == null) {
-                    return 0;
-                }
-                if (o1 == null) {
-                    return 1;
-                }
-                if (o2 == null) {
-                    return -1;
-                }
-                // Reverse order
-                return (int)(g.getCost(currentVertex, o2) - g.getCost(currentVertex, o1));
+        Arrays.sort(candidates, (o1, o2) -> {
+            if (o1 == null && o2 == null) {
+                return 0;
             }
+            if (o1 == null) {
+                return 1;
+            }
+            if (o2 == null) {
+                return -1;
+            }
+            // Reverse order
+            return (int)(g.getCost(currentVertex, o2) - g.getCost(currentVertex, o1));
         });
-        System.out.println("Candidates sorted : \n" + Arrays.toString(candidates));
+        //System.out.println("Candidates sorted : \n" + Arrays.toString(candidates));
 
     }
 
