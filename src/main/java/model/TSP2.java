ea1fa650028fa417ec8df33352b699c2bf15c412
abrissard <alexis.brissard@insa-lyon.fr>
2021-12-01 10:23:29 +0100

    :memo: Added description to TSP 2 + optimization

--- src/main/java/model/TSP2.java
+++ src/main/java/model/TSP2.java
@@ -1,38 +1,37 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
 public class TSP2 extends TSP1 {
+    /*
+    Custom bound method.
+    Evalute the lower bound by summing the minimal costs from current vertex and all unvisited vertices
+    to vertex 0 or any other unvisited vertices.
+     */
     @Override
     protected double bound(Integer currentVertex, Collection<Integer> unvisited, Collection<Integer> visited, Graph g) {
 
-        double sum = 0;
+        double evaluation = 0;
 
-
+        //Min cost for current vertex
         double minCostFromCurrent = Integer.MAX_VALUE;
-        for(Integer i : unvisited) {
-            //For current vertex
+        for(int i : unvisited) {
             if(g.getCost(currentVertex, i) < minCostFromCurrent) {
                 minCostFromCurrent = g.getCost(currentVertex, i);
             }
-        }
-        sum = sum + minCostFromCurrent;
 
-        for(Integer i : unvisited) {
-            //For unvisited vertex
+            //Min cost for unvisited vertex
             double minCost = g.getCost(i, 0);
-            for(Integer j : unvisited) {
-                if( (g.getCost(i, j) < minCost) && (!i.equals(j))  ) {
+            for(int j : unvisited) {
+                if( (g.getCost(i, j) < minCost) && (i!=j) ) {
                     minCost = g.getCost(i, j);
                 }
             }
-            sum = sum + minCost;
+            evaluation = evaluation + minCost;
         }
+        evaluation = evaluation + minCostFromCurrent;
 
-        return sum;
+        return evaluation;
     }
 
 
